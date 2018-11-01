e6a85388fff4907a52d6844e45573720d89ea87b
purespeed <rrit3880@gmail.com>
2018-11-01 19:23:39 +0100

    utilisation json et ViewPager

---
+++ app/src/main/java/com/francois/patrick/projet/controller/MoodFragment.java
@@ -0,0 +1,7 @@
+package com.francois.patrick.projet;
+
+/**
+ * Created by Patrick Francois on 29/10/2018.
+ */
+public class MoodFragment {
+}
---
+++ app/src/main/java/com/francois/patrick/projet/controller/MoodPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.francois.patrick.projet.controller;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by Patrick Francois on 29/10/2018.
+ */
+public class FragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private static int NUM_ITEMS = 3;
+
+    public FragmentPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    // Returns total number of pages
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    // Returns the fragment to display for that page
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: // Fragment # 0 - This will show FirstFragment
+                return FirstFragment.newInstance(0, "Page # 1");
+            case 1: // Fragment # 0 - This will show FirstFragment different title
+                return FirstFragment.newInstance(1, "Page # 2");
+            case 2: // Fragment # 1 - This will show SecondFragment
+                return SecondFragment.newInstance(2, "Page # 3");
+            default:
+                return null;
+        }
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Page " + position;
+    }
+}
